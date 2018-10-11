aa2a6e64a94313081e2c3fa759f5f91370146e61
Prateek Saxena <prtksxna@gmail.com>
2018-10-11 10:10:28 +0530

    Fix locked UI above headings
    Remove the CSS margin and padding hack and instead use JS
    to scrollTo the right position.
    
    Bug: T187672
    Change-Id: Icf29d49dded65cb116cdd636e0e796dfc916273e

--- static/scripts/open-member.js
+++ static/scripts/open-member.js
@@ -1,13 +1,24 @@
-if ( window.location.hash ) {
-  openMember();
-}
+window.addEventListener('DOMContentLoaded', function () {
+  if ( window.location.hash ) {
+    openMember();
+    window.setTimeout( function () {
+      window.scrollTo( 0, (window.pageYOffset || document.documentElement.scrollTop) - 72);
+    }, 0)
+  }
+});
 
-window.addEventListener( 'hashchange', openMember )
+window.addEventListener( 'hashchange', function (e) {
+  e.stopPropagation();
+  openMember();
+} );
 
 function openMember() {
-  var id = window.location.hash.substr(1);
-  var toggler = document.getElementById( 'toggle-' + id );
+  var id = window.location.hash.substr(1),
+    toggler = document.getElementById( 'toggle-' + id ),
+    offset = toggler.getBoundingClientRect().top,
+    y = window.scrollY + offset - 62 - 10;
   if ( toggler ) {
     toggler.checked = true;
   }
+  window.scrollTo(0, y);
 }
