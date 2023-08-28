a59e669817b3ee32a0123a20d331896f9d81164d
Ed Sanders <esanders@wikimedia.org>
2023-08-28 15:39:49 +0100

    build: Move client libraries to /lib to simplify eslint config
    Change-Id: I4a2c9652971aa59631811aa4d6245b63173fd9e1

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,7 @@ module.exports = function gruntConfig( grunt ) {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
 		eslint: {
-			src: [
-				'Gruntfile.js',
-				'betterlinks.js',
-				'lunrHelper.js',
-				'publish.js',
-				'static/scripts/fonts-loader.js',
-				'static/scripts/linenumber.js',
-				'static/scripts/lunrSearch.js',
-				'static/scripts/method-filters.js',
-				'static/scripts/open-members.js'
-			]
+			src: [ '.' ]
 		},
 
 		stylelint: {
--- static/scripts/open-member.js
+++ static/scripts/open-member.js
@@ -1,24 +1,24 @@
-window.addEventListener('DOMContentLoaded', function () {
-  if ( window.location.hash ) {
-    openMember();
-    window.setTimeout( function () {
-      window.scrollTo( 0, (window.pageYOffset || document.documentElement.scrollTop) - 72);
-    }, 0)
-  }
-});
+window.addEventListener( 'DOMContentLoaded', function () {
+	if ( window.location.hash ) {
+		openMember();
+		window.setTimeout( function () {
+			window.scrollTo( 0, ( window.pageYOffset || document.documentElement.scrollTop ) - 72 );
+		}, 0 );
+	}
+} );
 
-window.addEventListener( 'hashchange', function (e) {
-  e.stopPropagation();
-  openMember();
+window.addEventListener( 'hashchange', function ( e ) {
+	e.stopPropagation();
+	openMember();
 } );
 
 function openMember() {
-  var id = window.location.hash.substr(1),
-    toggler = document.getElementById( 'toggle-' + id ),
-    offset = toggler.getBoundingClientRect().top,
-    y = window.scrollY + offset - 62 - 10;
-  if ( toggler ) {
-    toggler.checked = true;
-  }
-  window.scrollTo(0, y);
+	var id = window.location.hash.slice( 1 ),
+		toggler = document.getElementById( 'toggle-' + id ),
+		offset = toggler.getBoundingClientRect().top,
+		y = window.scrollY + offset - 62 - 10;
+	if ( toggler ) {
+		toggler.checked = true;
+	}
+	window.scrollTo( 0, y );
 }
