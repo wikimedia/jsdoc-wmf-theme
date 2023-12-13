ae0d4e9c12b4277164c8a18b7c2ae7e566c3572d
Eric Gardner <gardner.ec@gmail.com>
2023-12-13 15:28:48 -0800

    js: Add redirect.js
    Introduces a basic redirect script that looks for hash fragments
    starting with #! (hashbangs). If such a fragment is contained in
    the user's incoming URL, the script will attempt to redirect them
    to the equivalent location in the new site.
    
    Bug: T352320
    Change-Id: I717c6a15803670e75730536da6c1db62e3a8e059

---
+++ static/scripts/redirect.js
@@ -0,0 +1,19 @@
+/**
+ * Redirect any inbound #! URLs to the old docs from JSDuck.
+ * We wnat this to run as soon as possible; no need to wait
+ * for DOMContentLoaded / load / etc.
+ */
+( () => {
+	const hash = window.location.hash;
+	const base = window.location.origin + window.location.pathname;
+
+	if ( hash.startsWith( '#!' ) ) {
+		const regex = /#!\/api\/(.+)/;
+		const match = hash.match( regex );
+
+		if ( match ) {
+			const extracted = match[ 1 ];
+			window.location.href = new URL( `${extracted}.html`, base );
+		}
+	}
+} )();
