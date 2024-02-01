c7405a7a983956da0f487ecec4e7fe9ee0f869c7
Derk-Jan Hartman <hartman.wiki@gmail.com>
2024-02-01 19:36:26 +0100

    Fix hash navigation for non-existing elements
    Not all inbound anchors start with toggle-
    
    Bug: T354763
    Change-Id: I579d3c78badd9d2acb9a5d0dfa543220aededa3c

--- static/scripts/open-member.js
+++ static/scripts/open-member.js
@@ -13,12 +13,12 @@ window.addEventListener( 'hashchange', function ( e ) {
 } );
 
 function openMember() {
-	var id = window.location.hash.slice( 1 ),
-		toggler = document.getElementById( 'toggle-' + id ),
-		offset = toggler.getBoundingClientRect().top,
-		y = window.scrollY + offset;
+	const id = window.location.hash.slice( 1 );
+	const toggler = document.getElementById( 'toggle-' + id );
 	if ( toggler ) {
+		const offset = toggler.getBoundingClientRect().top;
+		const y = window.scrollY + offset;
 		toggler.open = true;
+		window.scrollTo( 0, y );
 	}
-	window.scrollTo( 0, y );
 }
