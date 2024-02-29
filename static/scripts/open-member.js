0331000245a032e3a2a1004f2a2864199e8a7960
Anne Tomasevich <atomasevich@wikimedia.org>
2024-02-29 14:50:43 -0500

    Ensure proper scroll position for anchor links
    This patch ensures that the page is scrolled to exactly the
    right position when an anchor link is visited under various
    circumstances:
    - Whether it's a new page load or not
    - Whether it's a heading or a code line
    
    This is done by removing workaround elsewhere and standardizing
    on a `scroll-padding-top` rule on the html element, based on
    the height of the header.
    
    Bug: T356354
    Change-Id: I5fc4bf361464c6e29ea69776718187e25f041ce7

--- static/scripts/open-member.js
+++ static/scripts/open-member.js
@@ -2,7 +2,7 @@ window.addEventListener( 'DOMContentLoaded', function () {
 	if ( window.location.hash ) {
 		openMember();
 		window.setTimeout( function () {
-			window.scrollTo( 0, ( window.pageYOffset || document.documentElement.scrollTop ) - 72 );
+			window.scrollTo( 0, ( window.pageYOffset || document.documentElement.scrollTop ) );
 		}, 0 );
 	}
 } );
@@ -16,8 +16,7 @@ function openMember() {
 	const id = window.location.hash.slice( 1 );
 	const toggler = document.getElementById( 'toggle-' + id );
 	if ( toggler ) {
-		const offset = toggler.getBoundingClientRect().top;
-		const y = window.scrollY + offset;
+		const y = window.scrollY;
 		toggler.open = true;
 		window.scrollTo( 0, y );
 	}
