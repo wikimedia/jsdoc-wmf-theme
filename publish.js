740f849ddee13036d6ec80ec57c2f0129e69c6ce
Ed Sanders <esanders@wikimedia.org>
2024-05-03 10:41:45 +0100

    Indication optional parameters with square brackets
    Bug: T361154
    Change-Id: Idf0818029f9fc66320a5ab4a4656a381609b4551

--- publish.js
+++ publish.js
@@ -159,6 +159,10 @@ function updateItemName( item ) {
 		itemName = '&hellip;' + itemName;
 	}
 
+	if ( item.optional ) {
+		itemName = '[' + itemName + ']';
+	}
+
 	if ( attributes && attributes.length ) {
 		itemName = util.format( '%s<span class="signature-attributes">%s</span>', itemName,
 			attributes.join( ', ' ) );
