0d712f74d270dcb169d576de9de02762eb5d631c
Ed Sanders <esanders@wikimedia.org>
2024-05-03 10:32:43 +0100

    Create a single "default" plugin that loads the standard list
    Bug: T364104
    Change-Id: Iee3f753501ab7dd95adafd552ed8286cb58bbf43

---
+++ plugins/default.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// eslint-disable-next-line n/no-missing-require
+const app = require( 'jsdoc/app' ),
+	// eslint-disable-next-line n/no-missing-require
+	plugins = require( 'jsdoc/plugins' ),
+	// eslint-disable-next-line n/no-missing-require
+	path = require( 'jsdoc/path' ),
+	defaultPlugins = require( '../defaultPlugins' );
+
+plugins.installPlugins(
+	defaultPlugins.map(
+		( plugin ) => path.getResourcePath(
+			path.dirname( plugin ),
+			path.basename( plugin )
+		)
+	),
+	app.jsdoc.parser
+);
